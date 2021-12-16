29f5f53627fdd5049d50e8cbb817bdbe7ae6188e
AvniMittal13 <avni.mittal2002@gmail.com>
2021-12-16 23:00:14 +0530

    All specifications added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express=require("express");
+const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
@@ -7,50 +7,60 @@ const PythonShell = require('python-shell').PythonShell;
 const spawn = require("child_process").spawn;
 
 
-const app=express();
+const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const ny2o_key = '3X32YF-VDRZFK-35RFJS-4TFW'
-const nasa_key='4EhtxrAK41OTThAXAB5mDZe1fiYAPh7cjS8Ra6qb'
+const nasa_key = '4EhtxrAK41OTThAXAB5mDZe1fiYAPh7cjS8Ra6qb'
 const base = 'https://tle.ivanstanojevic.me/api/tle/'
 
 var satName = ''
 var satDate = ''
 var satL1 = ''
-var satL2= ''
-var q2_ans=[
+var satL2 = ''
+var q2_ans = [
     {
-    "Latitude":"",
-    "Longitude":"",
-    "Altitude":""
-}]
-q_lat= ''
-q_lon= ''
-day=''
-month=''
-year=''
+        "Latitude": "",
+        "Longitude": "",
+        "Altitude": ""
+    }]
+var q3_ans = [
+    {
+        "ans": ""
+    }
+]
+var q4_ans = [
+    {
+        "ans": ""
+    }
+]
+q_lat = ''
+q_lon = ''
+day = ''
+month = ''
+year = ''
 
 
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
 
     res.render("index")
 
 });
 
-app.post("/",function(req,res){
+app.post("/", function (req, res) {
 
     sat_code = req.body.satellite_code;
-    q_lat= req.body.que_lat
-    q_lon= req.body.que_lon
-    var in_Date=new Date(req.body.date_observe)
-    day=in_Date.getDate()
-    month=in_Date.getMonth()
-    year=in_Date.getFullYear()
+    q_lat = req.body.que_lat
+    q_lon = req.body.que_lon
+    var in_Date = new Date(req.body.date_observe)
+    day = in_Date.getDate()
+    month = in_Date.getMonth()
+    year = in_Date.getFullYear()
     // sat_name = req.body.satellite_name;
 
     // console.log(req.body)
@@ -61,21 +71,21 @@ app.post("/",function(req,res){
     // } else if (sat_name!='' && sat_code=='') {
     // const url = base+sat_code+"?search="+sat_name;
 
-    const url = base+sat_code;
-    https.get(url,function(response){
+    const url = base + sat_code;
+    https.get(url, function (response) {
         // console.log(response.statusCode);
 
-        response.on("data",function(data){
-            sat_data=JSON.parse(data)
-            
-            satId= sat_data.satelliteId
+        response.on("data", function (data) {
+            sat_data = JSON.parse(data)
+
+            satId = sat_data.satelliteId
             satName = sat_data.name
             satDate = sat_data.date
             satL1 = sat_data.line1
-            satL2= sat_data.line2
-            
-            
-            console.log(q_lat,q_lon,day,month,year)
+            satL2 = sat_data.line2
+
+
+            console.log(q_lat, q_lon, day, month, year)
 
             console.log(sat_data)
             // console.log('hello')
@@ -85,63 +95,65 @@ app.post("/",function(req,res){
                 // pythonPath: 'path/to/python',
                 // pythonOptions: ['-u'],
                 // scriptPath: 'path/to/my/scripts',
-                args: [satName, satL1, satL2,q_lat,q_lon,day,month,year]
-              };
-              
-              PythonShell.run('orbit_calculation.py', options, function (err, results) {
-                if (err) 
-                  throw err;
+                args: [satName, satL1, satL2, q_lat, q_lon, day, month, year]
+            };
+
+            PythonShell.run('orbit_calculation.py', options, function (err, results) {
+                if (err)
+                    throw err;
                 //   console.log('results: %j', results);
                 //   console.log(results.split("\\","\\"))
-                  console.log(results[0])
-                  console.log(results[1])
-                  q2_ans.push(JSON.parse(results[1]))
+                console.log(results[0])
+                console.log(results[1])
+                q2_ans.push(JSON.parse(results[1]))
+                q3_ans.push(JSON.parse(results[2]))
+                q4_ans.push(JSON.parse(results[3]))
                 //   console.log(q2_ans)
                 //   console.log(q2_ans.Latitude)
-                  
+
                 console.log(q2_ans)
 
                 res.redirect("/simulation")
-                  
-              });
+
+            });
 
 
-            
+
             // PythonShell.run('orbit_calculation.py', null, function (err) {
             //     if (err) throw err;
             //     console.log('finished');
             //   });
         })
 
-    } )
+    })
     // }
 
 })
 
 
-app.get("/simulation",function(req,res){
+app.get("/simulation", function (req, res) {
 
-    
+
     // const pythonProcess = spawn('python',["orbit_calculation.py",satName, satL1, satL2]);
 
-    
+
     // pythonProcess.stdout.on('data', function(data){
     //     // Do something with the data returned from python script
     //     console.log(data)
     // });
 
-    
+
 
 
     // console.log(satL1)
     // console.log(satL2)
-    
-    res.render("simulation",{sat_name:satName,l1:satL1,l2:satL2,q2:q2_ans})
+
+    res.render("simulation", { sat_name: satName, l1: satL1, l2: satL2, q2: q2_ans, q3: q3_ans, q4: q4_ans })
 
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
 
     console.log("Server is up and running");
-    
+
 });
--- orbit_calculation.py
+++ orbit_calculation.py
@@ -1,5 +1,15 @@
 # pip install sgp4
+from astropy.constants import G, M_earth
+import numpy as np
+from pytz import timezone
+from datetime import timedelta
+from skyfield.api import load, wgs84
+from skyfield.api import EarthSatellite
+import scipy
+from pprint import pprint
+from sgp4 import exporter
 import sys
+from numpy import add
 from sgp4.api import Satrec
 import pandas as pd
 import json
@@ -8,34 +18,28 @@ from sgp4.functions import days2mdhms
 
 sat_name = sys.argv[1]
 l1 = sys.argv[2]
-l2=sys.argv[3]
-que_lat=sys.argv[4]
-que_lon=sys.argv[5]
-day=sys.argv[6]
-month=sys.argv[7]
-year=sys.argv[8]
+l2 = sys.argv[3]
+que_lat = sys.argv[4]
+que_lon = sys.argv[5]
+day = sys.argv[6]
+month = sys.argv[7]
+year = sys.argv[8]
 
 # print(que_lat,que_lon)
 
 satellite = Satrec.twoline2rv(l1, l2)
 
-from sgp4 import exporter
-from pprint import pprint
 
 fields = exporter.export_omm(satellite, sat_name)
 print(fields)    # printing satellite info obtained from TLE  ====1====
 
 Orbit_Period = (24*3600)/fields['MEAN_MOTION']
 
-from astropy.constants import G,M_earth
-import scipy
 u = G*M_earth
-a= ((((Orbit_Period)**2)*u)/(4*(scipy.pi)**2))**(1/3)
+a = ((((Orbit_Period)**2)*u)/(4*(scipy.pi)**2))**(1/3)
 
 # pip install skyfield
 
-from skyfield.api import EarthSatellite
-from skyfield.api import load, wgs84
 
 ts = load.timescale()
 satellite_f = EarthSatellite(l1, l2, sat_name, ts)
@@ -44,7 +48,7 @@ satellite_f = EarthSatellite(l1, l2, sat_name, ts)
 # print(satellite_f.epoch.utc_jpl())
 
 # PART 2 --- finding longitude, latitude and altitude
-t=ts.now()
+t = ts.now()
 geocentric = satellite_f.at(t)
 # print(geocentric.position.km)
 
@@ -55,15 +59,65 @@ lat, lon = wgs84.latlon_of(geocentric)
 altitude = wgs84.height_of(geocentric)
 # print('Altitude: ',altitude.km)
 
-q2_data = {"Latitude": str(lat), "Longitude": str(lon), "Altitude": str(altitude.km) }
-# df_q2=pd.DataFrame(q2_data)
-# q2_res = df_q2.to_json(orient="table")
+q2_data = {"Latitude": str(lat), "Longitude": str(
+    lon), "Altitude": str(altitude.km)}
 print(json.dumps(q2_data))   # ======== 2 =======
 # json.loads(q2_data)
 
 # PART 3 --- given longitude and latitude, find when next visible
-# bluffton = wgs84.latlon(float(que_lat), float(que_lon))
-# t_check = ts.utc(float(year), float(month), float(day))
+bluffton = wgs84.latlon(float(que_lat), float(que_lon))
 
 
+eastern = timezone('US/Eastern')
+
+start_time = ts.now()
+t_inc = start_time
+y_n = 0
+
+
+range = np.arange(0, Orbit_Period, 20)
+t_arr = ts.utc(float(year), float(month), float(day), 0, 0, range)
+
+# e = eastern.localize(timedelta(hours=20/3600))
+# add = ts.from_datetime(e)
+
+for i in t_arr:
+    # t_inc = t_inc + add*i
+    difference = satellite_f - bluffton
+    topocentric = difference.at(i)
+    # geocentric = satellite_f.at(t_inc)
+    alt, az, distance = topocentric.altaz()
+    if(alt.degrees > 30 or alt.degrees < 150):
+        y_n = 1
+        break
+
+if(y_n == 1):
+    date_visible = str(i.utc.year) + '/' + \
+        str(i.utc.month) + '/' + str(i.utc.day)
+    ans3 = "Yes, satellite will be visible on "+date_visible+". Move " + \
+        str(distance) + " "+str(az.degrees) + "degrees clockwise from North"
+else:
+    ans3 = "Satellite is not visible from the given location"
+
+q3_data = {"ans": ans3}
+print(json.dumps(q3_data))   # ======== 3 =======
+
+
+# PART 4
+
+t_check = ts.utc(float(year), float(month), float(day))
+difference = satellite_f - bluffton
+topocentric = difference.at(t_check)
+# geocentric = satellite_f.at(t_inc)
+alt, az, distance = topocentric.altaz()
+if(alt.degrees > 30 or alt.degrees < 150):
+    ans = "Satellite is visible, Move " + \
+        str(distance) + " "+str(az.degrees) + "degrees clockwise from North"
+
+else:
+    ans = "Satellite is not visible"
+
+q4_data = {"ans": ans}
+print(json.dumps(q4_data))   # ======== 4 =======
+# print(Orbit_Period)
 # sys.stdout.flush()
